4a78682c1e3d782867547192986435a1ae2c0b98
Cong Wang <77494072+hydy1943cong@users.noreply.github.com>
2024-09-05 16:58:52 -0400

    able to leave comments

--- models/Comment.js
+++ models/Comment.js
@@ -18,6 +18,7 @@ Comment.init(
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
+      defaultValue: DataTypes.NOW,
     },
     user_id: {
       type: DataTypes.INTEGER,
--- public/js/comment.js
+++ public/js/comment.js
@@ -3,7 +3,7 @@ document.querySelector('#comment-form').addEventListener('submit', async (event)
   
     const content = document.querySelector('#comment-content').value.trim();
     const blogId = window.location.pathname.split('/').pop(); 
-  
+    console.log('Blog ID:', blogId);
     if (content) {
       const response = await fetch('/api/comments', {
         method: 'POST',
